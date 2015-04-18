1df74f2d270a8510383462cb3803542bc382691f
naveenchakri <naveenchakrib@gmail.com>
2015-04-18 12:25:02 +0530

    firstCommit

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ app.get('/', function(request, response) {
   response.send('Hello World!');
 });
 
+app.get('/naveen', function(request, response) {
+  response.send('Hello Naveen!');
+});
+
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
 });
